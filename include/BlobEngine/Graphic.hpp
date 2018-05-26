9da7efa13584bb81b28a3639d92bf8135ad8d5c1
Alexandre de LINGUA de SAINT BLANQUAT <alexandredlsb@gmail.com>
2018-05-26 12:52:58 +0200

    up

---
+++ include/BlobEngine/Graphic.hpp
@@ -0,0 +1,14 @@
+//
+// Created by alexa on 20/05/2018.
+//
+
+#ifndef BLOBENGINE_GRAPHIC_HPP
+#define BLOBENGINE_GRAPHIC_HPP
+
+
+class graphic {
+
+};
+
+
+#endif //BLOBENGINE_GRAPHIC_HPP
---
+++ src/graphic/Graphic.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexa on 20/05/2018.
+//
+
+#include "BlobEngine/Graphic.hpp"
