022fbf3e09a0b8e137ba88d06a2d90c4d9914bff
Alexdesaint <alexandredlsb@gmail.com>
2019-10-26 21:12:56 +0200

    add more colors

--- include/Blob/GL/Shapes.hpp
+++ include/Blob/GL/Shapes.hpp
@@ -5,24 +5,24 @@
 
 namespace Blob::GL::Shapes {
 
-	void init();
+    void init();
 
-	void destroy();
+    void destroy();
 
-	class Cube : public Renderable {
-	public:
-		explicit Cube();
-	};
+    class Cube : public Renderable {
+    public:
+        explicit Cube();
+    };
 
-	class Plane : public Renderable {
-	public:
-		explicit Plane();
-	};
+    class Plane : public Renderable {
+    public:
+        explicit Plane();
+    };
 
-	class OctagonalPrism : public Renderable {
-	public:
-		explicit OctagonalPrism();
-	};
+    class OctagonalPrism : public Renderable {
+    public:
+        explicit OctagonalPrism();
+    };
 }
 
 namespace Blob::GL {
@@ -35,7 +35,145 @@ namespace Blob::GL {
     };
 
     struct Colors {
-        static Texture *Blue, *Red, *Orange, *Yellow, *Green, *Pink;
+        static Texture *maroon,
+                *darkRed,
+                *brown,
+                *firebrick,
+                *crimson,
+                *red,
+                *tomato,
+                *coral,
+                *indianRed,
+                *lightCoral,
+                *darkSalmon,
+                *salmon,
+                *lightSalmon,
+                *orangeRed,
+                *darkOrange,
+                *orange,
+                *gold,
+                *darkGoldenRod,
+                *goldenRod,
+                *paleGoldenRod,
+                *darkKhaki,
+                *khaki,
+                *olive,
+                *yellow,
+                *yellowGreen,
+                *darkOliveGreen,
+                *oliveDrab,
+                *lawnGreen,
+                *chartReuse,
+                *greenYellow,
+                *darkGreen,
+                *green,
+                *forestGreen,
+                *lime,
+                *limeGreen,
+                *lightGreen,
+                *paleGreen,
+                *darkSeaGreen,
+                *mediumSpringGreen,
+                *springGreen,
+                *seaGreen,
+                *mediumAquaMarine,
+                *mediumSeaGreen,
+                *lightSeaGreen,
+                *darkSlateGray,
+                *teal,
+                *darkCyan,
+                *aqua,
+                *cyan,
+                *lightCyan,
+                *darkTurquoise,
+                *turquoise,
+                *mediumTurquoise,
+                *paleTurquoise,
+                *aquaMarine,
+                *powderBlue,
+                *cadetBlue,
+                *steelBlue,
+                *cornFlowerBlue,
+                *deepSkyBlue,
+                *dodgerBlue,
+                *lightBlue,
+                *skyBlue,
+                *lightSkyBlue,
+                *midnightBlue,
+                *navy,
+                *darkBlue,
+                *mediumBlue,
+                *blue,
+                *royalBlue,
+                *blueViolet,
+                *indigo,
+                *darkSlateBlue,
+                *slateBlue,
+                *mediumSlateBlue,
+                *mediumPurple,
+                *darkMagenta,
+                *darkViolet,
+                *darkOrchid,
+                *mediumOrchid,
+                *purple,
+                *thistle,
+                *plum,
+                *violet,
+                *magenta,
+                *orchid,
+                *mediumVioletRed,
+                *paleVioletRed,
+                *deepPink,
+                *hotPink,
+                *lightPink,
+                *pink,
+                *antiqueWhite,
+                *beige,
+                *bisque,
+                *blanchedAlmond,
+                *wheat,
+                *cornSilk,
+                *lemonChiffon,
+                *lightGoldenRodYellow,
+                *lightYellow,
+                *saddleBrown,
+                *sienna,
+                *chocolate,
+                *peru,
+                *sandyBrown,
+                *burlyWood,
+                *tan,
+                *rosyBrown,
+                *moccasin,
+                *navajoWhite,
+                *peachPuff,
+                *mistyRose,
+                *lavenderBlush,
+                *linen,
+                *oldLace,
+                *papayaWhip,
+                *seaShell,
+                *mintCream,
+                *slateGray,
+                *lightSlateGray,
+                *lightSteelBlue,
+                *lavender,
+                *floralWhite,
+                *aliceBlue,
+                *ghostWhite,
+                *honeydew,
+                *ivory,
+                *azure,
+                *snow,
+                *black,
+                *dimGrey,
+                *grey,
+                *darkGrey,
+                *silver,
+                *lightGrey,
+                *gainsboro,
+                *whiteSmoke,
+                *white;
     };
 }
 
