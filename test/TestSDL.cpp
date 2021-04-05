0fce533632164e8d6395305a44a4071c72344285
alexdesaint <alexandredlsb@gmail.com>
2021-04-05 20:08:54 +0200

    Fix TestSDL

--- test/TestSDL.cpp
+++ test/TestSDL.cpp
@@ -1,7 +1,4 @@
-// SDL2 Hello, World!
-// This should display a white screen for 2 seconds
-// compile with: clang++ main.cpp -o hello_sdl2 -lSDL2
-// run with: ./hello_sdl2
+#include "SDL_render.h"
 #include <SDL.h>
 #include <stdio.h>
 
@@ -9,20 +6,35 @@
 #define SCREEN_HEIGHT 480
 
 int main(int argc, char *args[]) {
-    SDL_Window *window = NULL;
-    SDL_Surface *screenSurface = NULL;
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
         return 1;
     }
-    window = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    SDL_Window *window = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
         fprintf(stderr, "could not create window: %s\n", SDL_GetError());
         return 1;
     }
-    screenSurface = SDL_GetWindowSurface(window);
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0, 0 ));
-    SDL_UpdateWindowSurface(window);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        fprintf(stderr, "could not create renderer: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+    SDL_RenderClear( renderer );
+
+    SDL_Rect r;
+    r.x = 50;
+    r.y = 50;
+    r.w = 50;
+    r.h = 50;
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderFillRect(renderer, &r);
+
+    SDL_RenderPresent(renderer);
 
     bool quit = false;
     while (!quit) {
@@ -40,53 +52,8 @@ int main(int argc, char *args[]) {
             }
         }
     }
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
-
-// {
-//     SDL_Window* window = NULL;
-//     window = SDL_CreateWindow
-//         (
-//             "Jeu de la vie", SDL_WINDOWPOS_UNDEFINED,
-//             SDL_WINDOWPOS_UNDEFINED,
-//             640,
-//             480,
-//             SDL_WINDOW_SHOWN
-//         );
-
-//     // Setup renderer
-//     SDL_Renderer* renderer = NULL;
-//     renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
-
-//     // Set render color to red ( background will be rendered in this color )
-//     SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
-
-//     // Clear winow
-//     SDL_RenderClear( renderer );
-
-//     // Creat a rect at pos ( 50, 50 ) that's 50 pixels wide and 50 pixels high.
-//     SDL_Rect r;
-//     r.x = 50;
-//     r.y = 50;
-//     r.w = 50;
-//     r.h = 50;
-
-//     // Set render color to blue ( rect will be rendered in this color )
-//     SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
-
-//     // Render rect
-//     SDL_RenderFillRect( renderer, &r );
-
-//     // Render the rect to the screen
-//     SDL_RenderPresent(renderer);
-
-//     // Wait for 5 sec
-//     SDL_Delay( 5000 );
-
-//     SDL_DestroyWindow(window);
-//     SDL_Quit();
-
-//     return EXIT_SUCCESS;
-// }
